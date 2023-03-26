1c6cd3f20bb7cbac83a5c918482c4db368cb9df3
yish <shlee4458@gmail.com>
2023-03-26 16:27:17 -0400

    Create a project

---
+++ src/test/java/com/yish/employeeManagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yish.employeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
